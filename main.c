ce9a1fbe32a953eb0d2b28ef17ec9a102e459c02
Eina Safor Coma Bages <ecoma-ba@car3s2.42barcelona.com>
2024-07-22 16:50:03 +0200

    proper objectiu buscar exes

--- main.c
+++ main.c
@@ -6,21 +6,26 @@
 /*   By: ecoma-ba <ecoma-ba@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 14:57:14 by ecoma-ba          #+#    #+#             */
-/*   Updated: 2024/07/22 15:57:07 by ecoma-ba         ###   ########.fr       */
+/*   Updated: 2024/07/22 16:28:30 by ecoma-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include "ft_printf.h"
+#include "libft.h"
+#include <unistd.h>
 
-int main(int argc, char *argv[], char *envp[])
+extern char	**environ;
+
+int	main(int argc, char *argv[])
 {
-	int i = -1;
+	char	**envp;
+	int		i;
+
+	envp = environ;
+	i = -1;
 	while (++i < argc)
 		ft_printf("argv[%d]: %s\n", i, argv[i]);
-	
 	i = -1;
 	while (envp[++i])
 		ft_printf("envp[%d]: %s\n", i, envp[i]);
-
 }
